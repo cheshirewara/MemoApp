abf46a162f54b62377aab79141472b1ba8df9937
cheshire boy <info@cheshire-wara.com>
2021-06-14 00:05:25 +0900

    crate memoedit and memocreate screen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreate';
 
 export default function App() {
   return (
-    <MemoListScreen />
-  // <MemoDetailScreen />
+    // <MemoListScreen />
+    // <MemoDetailScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
