e2d84a2feb9a38697e87c5a294f83625f26aa5fe
cheshire boy <info@cheshire-wara.com>
2021-06-16 07:32:35 +0900

    save memo firestore

--- src/screens/MemoCreate.jsx
+++ src/screens/MemoCreate.jsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View, TextInput, StyleSheet,
 } from 'react-native';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyboardSafeView';
 
 export default function MemoCreateScreen(props) {
+  // constructor
   const { navigation } = props;
+
+  // screen objext
+  const [bodyText, setBodyText] = useState('');
+
+  // handler
+  function handlePress() {
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    const ref = db.collection(`users/${currentUser.uid}/memos`);
+    ref.add({
+      bodyText,
+      updatedAt: new Date(),
+    })
+      .then((docRef) => {
+        console.log('Create:', docRef.id);
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+  }
+
   return (
     <KeyboardSafeView style={styles.container}>
       {/* <AppBar /> */}
 
       <View style={styles.inputContainer}>
-        <TextInput value="" multiline style={styles.input} />
+        <TextInput
+          style={styles.input}
+          value={bodyText}
+          multiline
+          autoFocus
+          onChangeText={(text) => { setBodyText(text); }}
+        />
       </View>
 
-      <CircleButton name="check" onPress={() => { navigation.goBack(); }} />
+      <CircleButton
+        name="check"
+        onPress={handlePress}
+      />
     </KeyboardSafeView>
   );
 }
